60d4bcf6e700eaed6b215843501bef94f705690b
Ali Abbas Jaffri <ali.abbasjaffri@live.com>
2016-04-19 01:06:29 +0500

    Stable deploy of Webservice.

---
+++ src/main/java/com/restfulservices/HelloWorld.java
@@ -0,0 +1,20 @@
+package com.restfulservices;
+
+/**
+ * Created by aliabbasjaffri on 18/04/2016.
+ */
+
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class HelloWorld {
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getMessage() {
+        return "Hello world fraaaandddddd!";
+    }
+}
