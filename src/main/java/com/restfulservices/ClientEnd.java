9653e3140bcd3b335b85a921d4f42fdb0df16fab
Ali Abbas Jaffri <ali.abbasjaffri@live.com>
2016-04-20 01:38:14 +0500

    Client Side Implementation.

---
+++ src/main/java/com/restfulservices/ClientEnd.java
@@ -0,0 +1,49 @@
+package com.restfulservices;
+
+
+import java.net.URI;
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriBuilder;
+
+/**
+ * Created by aliabbasjaffri on 20/04/2016.
+ */
+public class ClientEnd
+{
+    public static void main(String[] args) {
+        try {
+
+            Client client = Client.create();
+
+            WebResource webResource = client
+                    .resource(getBaseURI());
+
+            ClientResponse response = webResource.accept(MediaType.APPLICATION_JSON)
+                    .get(ClientResponse.class);
+
+            if (response.getStatus() != 200) {
+                throw new RuntimeException("Failed : HTTP error code : "
+                        + response.getStatus());
+            }
+
+            String output = response.getEntity(String.class);
+
+            System.out.println("Output from Server .... \n");
+            System.out.println(output);
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+
+        }
+
+    }
+
+    private static URI getBaseURI() {
+        return UriBuilder.fromUri("http://localhost:8080/ftocservice").build();
+    }
+}
